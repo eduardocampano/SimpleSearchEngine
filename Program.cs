17f589e9ee9c02a8ff1d79dbd8bca33661fde859
Eduardo Campañó <campano.eduardo@gmail.com>
2012-10-04 17:03:05 -0300

    First commit
    Comparisson beetween the simple search engine and search using LINQ over
    about 5K movie titles from IMDB

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace SimpleSearchEngine
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var searchEngine = new SearchEngine<Movie>();
+
+			var documents = GetDocuments();
+			
+			searchEngine.Index(documents, x => x.Fields.Title);
+			var documentsForSimpleSearch = documents.Where(x => !string.IsNullOrWhiteSpace(x.Fields.Title))
+													.Select(x => new { Index = x.Fields.Title.Split(new [] { ' ', '\n', '\r', '\t', '-' }, StringSplitOptions.RemoveEmptyEntries), Movie = x })
+													.ToList();
+
+			while (true)
+			{
+				Console.Write("Enter search term: ");
+				var searchTerm = Console.ReadLine();
+
+				if (string.IsNullOrEmpty(searchTerm))
+					break;
+
+				// ---------- SEARCH ENGINE SEARCH ----------------
+				Console.WriteLine("Using search engine");
+				Console.WriteLine("===================");
+
+				var searchResults = searchEngine.Search(searchTerm);
+
+				foreach (var result in searchResults)
+					Console.WriteLine("*** Matched {0}", result.Fields.Title);
+
+				Console.WriteLine("{0} results ({1} seconds)", searchResults.Count(), searchResults.Elapsed.TotalSeconds.ToString("0.00####################"));
+				Console.WriteLine();
+				
+				// ---------- LINQ SEARCH ----------------
+				Console.WriteLine("Using LINQ");
+				Console.WriteLine("==========");
+
+				var stopwatch = Stopwatch.StartNew();
+				var results = documentsForSimpleSearch.Where(x => x.Index.Any(e => e.StartsWith(searchTerm, StringComparison.InvariantCultureIgnoreCase))).ToList();
+				stopwatch.Stop();
+
+				foreach (var result in results)
+					Console.WriteLine("*** Matched {0}", result.Movie.Fields.Title);
+
+				Console.WriteLine("{0} results ({1} seconds)", results.Count(), stopwatch.Elapsed.TotalSeconds.ToString("0.00####################"));
+				Console.WriteLine();
+			}
+		}
+
+		static Movie[] GetDocuments()
+		{
+			var json = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "moviedata.json"));
+			return JsonConvert.DeserializeObject<Movie[]>(json);
+		}
+
+		class Movie
+		{
+			public string Type { get; set; }
+			public string Id { get; set; }
+			public int Version { get; set; }
+			public string Lang { get; set; }
+			public Field Fields { get; set; }
+		}
+
+		class Field
+		{
+			public string Title { get; set; }
+			public int? Year { get; set; }
+			public string Director { get; set; }
+			public string[] Genere { get; set; }
+			public string[] Actor { get; set; }
+		}
+	}
+}
---
+++ SearchEngine.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace SimpleSearchEngine
+{
+	public class SearchEngine<T>
+	{
+		private Node _root;
+
+		public SearchEngine()
+		{
+		}
+
+		public SearchEngine(IEnumerable<T> elementsToIndex, Func<T, string> indexer)
+		{
+			Index(elementsToIndex, indexer);	
+		}
+
+		public SearchResult Search(string text)
+		{
+			var stopwatch = Stopwatch.StartNew();
+			var node = FindNode(text.ToLowerInvariant());
+			stopwatch.Stop();
+
+			return new SearchResult(node == null ? new HashSet<T>() : node.Elements, stopwatch.Elapsed);
+		}
+
+		public void Index(IEnumerable<T> elements, Func<T, string> indexer)
+		{
+			_root = null;
+			var tree = new SearchEngine<T>();
+
+			var entries = new Dictionary<string, HashSet<T>>();
+			foreach (var element in elements)
+			{
+				var index = indexer(element);
+				if (string.IsNullOrWhiteSpace(index))
+					continue;
+
+				var indexEntries = new List<string>();
+				indexEntries.Add(index);
+				indexEntries.AddRange(index.Split(new[] { ' ', '\n', '\r', '\t', '-' }, StringSplitOptions.RemoveEmptyEntries));
+
+				foreach (var indexEntry in indexEntries)
+				{
+					var key = indexEntry.ToLowerInvariant();
+					if (entries.ContainsKey(key))
+						entries[key].Add(element);
+					else
+						entries.Add(key, new HashSet<T>() { element });
+				}
+			}
+
+			var indexKeys = entries.Select(x => x.Key).ToArray();
+
+			tree.BuildBalancedTree(indexKeys, 0, indexKeys.Length);
+
+			foreach (var entry in entries)
+				tree.AddToLists(entry.Key, entry.Value);
+
+			_root = tree._root;
+		}
+
+		private void BuildBalancedTree(string[] suggestions, int left, int right)
+		{
+			if (left < right)
+			{
+				int mid = (left + right) / 2;
+				_root = Insert(suggestions[mid], 0, _root);
+				BuildBalancedTree(suggestions, left, mid);
+				BuildBalancedTree(suggestions, mid + 1, right);
+			}
+		}
+
+		private Node Insert(string suggestion, int i, Node ls)
+		{
+			if (ls == null)
+				return new Node(suggestion, suggestion.Length, null);
+			if (suggestion[i] < ls.First[i])
+				ls.Left = Insert(suggestion, i, ls.Left);
+			else if (suggestion[i] > ls.First[i])
+				ls.Right = Insert(suggestion, i, ls.Right);
+			else
+			{
+				while (++i < ls.CharEnd)
+				{
+					if (i == suggestion.Length || suggestion[i] != ls.First[i])
+					{
+						ls.Mid = new Node(ls.First, ls.CharEnd, ls.Mid);
+						ls.CharEnd = i;
+						break;
+					}
+				}
+				if (i < suggestion.Length)
+					ls.Mid = Insert(suggestion, i, ls.Mid);
+			}
+			return ls;
+		}
+
+		private void AddToLists(string suggestion, IEnumerable<T> elements)
+		{
+			int i = 0;
+			Node ls = _root;
+			while (true)
+			{
+				if (suggestion[i] < ls.First[i])
+					ls = ls.Left;
+				else if (suggestion[i] > ls.First[i])
+					ls = ls.Right;
+				else
+				{
+					foreach (var el in elements)
+						ls.Elements.Add(el);
+					i = ls.CharEnd;
+					if (i == suggestion.Length)
+						return;
+					ls = ls.Mid;
+				}
+			}
+		}
+
+		private Node FindNode(string prefix)
+		{
+			int i = 0;
+			Node ls = _root;
+			while (ls != null)
+			{
+				if (prefix[i] < ls.First[i])
+					ls = ls.Left;
+				else if (prefix[i] > ls.First[i])
+					ls = ls.Right;
+				else
+				{
+					while (++i < ls.CharEnd)
+					{
+						if (i == prefix.Length)
+							return ls;
+						if (prefix[i] != ls.First[i])
+							return null;
+					}
+					if (i == prefix.Length)
+						return ls;
+					ls = ls.Mid;
+				}
+			}
+			return null;
+		}
+
+		public class SearchResult : IEnumerable<T>
+		{
+			private readonly IEnumerable<T> _results;
+
+			internal SearchResult(IEnumerable<T> results, TimeSpan elapsed)
+			{
+				_results = results;
+				Elapsed = elapsed;
+			}
+
+			public TimeSpan Elapsed { get; private set; }
+
+			public IEnumerator<T> GetEnumerator()
+			{
+				return _results.GetEnumerator();
+			}
+
+			IEnumerator IEnumerable.GetEnumerator()
+			{
+				return GetEnumerator();
+			}
+		}
+
+		private class Node
+		{
+			public string First { get; private set; }
+			public HashSet<T> Elements { get; private set; }
+			public int CharEnd { get; set; }
+			public Node Left { get; set; }
+			public Node Mid { get; set; }
+			public Node Right { get; set; }
+
+			public Node(string first, int charEnd, Node mid)
+			{
+				First = first;
+				Elements = new HashSet<T>();
+				CharEnd = charEnd;
+				Mid = mid;
+			}
+		}
+	}
+}
