750c72ed0e2013602211954dbb679aa74446e6b8
Eduardo Campañó <campano.eduardo@gmail.com>
2022-07-07 09:46:46 -0300

    Upgraded to .net 6 to remove dependent library security warnings

--- Program.cs
+++ Program.cs
@@ -1,97 +1,84 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using Newtonsoft.Json;
+﻿using System.Diagnostics;
+using System.Text.Json;
+using SimpleSearchEngine;
 
-namespace SimpleSearchEngine
-{
-	class Program
+var searchEngine = new SearchEngine<Movie>();
+
+var movies = GetMovies();
+searchEngine.Index(movies, x => x.Fields.Title);
+
+var linqIndex = movies.Where(x => !string.IsNullOrWhiteSpace(x.Fields.Title))
+	.Select(x => new LinqIndexEntry()
 	{
-		static void Main(string[] args)
-		{
-			var searchEngine = new SearchEngine<Movie>();
+		Words = x.Fields.Title.Split(new[] { ' ', '\n', '\r', '\t', '-' }, StringSplitOptions.RemoveEmptyEntries),
+		Movie = x
+	})
+	.ToList();
 
-			var documents = GetDocuments();
-			searchEngine.Index(documents, x => x.Fields.Title);
+while (true)
+{
+	Console.Write("Enter search term: ");
+	var searchTerm = Console.ReadLine();
 
-			var compare = args.Any() && args[0].Equals("compare", StringComparison.InvariantCultureIgnoreCase);
-			List<LinqIndexEntry> linqIndex = null;
-			if (compare)
-			{
-				linqIndex = documents.Where(x => !string.IsNullOrWhiteSpace(x.Fields.Title))
-														.Select(x => new LinqIndexEntry() { Words = x.Fields.Title.Split(new[] { ' ', '\n', '\r', '\t', '-' }, StringSplitOptions.RemoveEmptyEntries), Movie = x })
-														.ToList();
-			}
+	if (string.IsNullOrEmpty(searchTerm))
+		break;
 
-			while (true)
-			{
-				Console.Write("Enter search term: ");
-				var searchTerm = Console.ReadLine();
+	// ---------- SEARCH ENGINE SEARCH ----------------
+	Console.WriteLine("Using search engine");
+	Console.WriteLine("===================");
 
-				if (string.IsNullOrEmpty(searchTerm))
-					break;
+	var searchResults = searchEngine.Search(searchTerm);
 
-				// ---------- SEARCH ENGINE SEARCH ----------------
-				Console.WriteLine("Using search engine");
-				Console.WriteLine("===================");
+	foreach (var result in searchResults)
+		Console.WriteLine("*** {0}", result.Fields.Title);
 
-				var searchResults = searchEngine.Search(searchTerm);
+	Console.WriteLine("{0} results ({1} seconds)", searchResults.Count(), searchResults.Elapsed.TotalSeconds.ToString("0.00####################"));
+	Console.WriteLine();
 
-				foreach (var result in searchResults)
-					Console.WriteLine("*** {0}", result.Fields.Title);
+	// ---------- LINQ SEARCH ----------------
+	Console.WriteLine("Using LINQ");
+	Console.WriteLine("==========");
 
-				Console.WriteLine("{0} results ({1} seconds)", searchResults.Count(), searchResults.Elapsed.TotalSeconds.ToString("0.00####################"));
-				Console.WriteLine();
-				
-				// ---------- LINQ SEARCH ----------------
-				if (compare)
-				{
-					Console.WriteLine("Using LINQ");
-					Console.WriteLine("==========");
+	var stopwatch = Stopwatch.StartNew();
+	var results = linqIndex.Where(x => x.Words.Any(e => e.StartsWith(searchTerm, StringComparison.InvariantCultureIgnoreCase))).ToList();
+	stopwatch.Stop();
 
-					var stopwatch = Stopwatch.StartNew();
-					var results = linqIndex.Where(x => x.Words.Any(e => e.StartsWith(searchTerm, StringComparison.InvariantCultureIgnoreCase))).ToList();
-					stopwatch.Stop();
+	foreach (var result in results)
+		Console.WriteLine("*** {0}", result.Movie.Fields.Title);
 
-					foreach (var result in results)
-						Console.WriteLine("*** {0}", result.Movie.Fields.Title);
+	Console.WriteLine("{0} results ({1} seconds)", results.Count(), stopwatch.Elapsed.TotalSeconds.ToString("0.00####################"));
+	Console.WriteLine();
+}
 
-					Console.WriteLine("{0} results ({1} seconds)", results.Count(), stopwatch.Elapsed.TotalSeconds.ToString("0.00####################"));
-					Console.WriteLine();
-				}
-			}
-		}
+static Movie[] GetMovies()
+{
+	var json = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "moviedata.json"));
+	return JsonSerializer.Deserialize<Movie[]>(json, new JsonSerializerOptions()
+	{
+		PropertyNameCaseInsensitive = true
+	}) ?? Array.Empty<Movie>();
+}
 
-		static Movie[] GetDocuments()
-		{
-			var json = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "moviedata.json"));
-			return JsonConvert.DeserializeObject<Movie[]>(json);
-		}
+class LinqIndexEntry
+{
+	public string[] Words { get; set; }
+	public Movie Movie { get; set; }
+}
 
-		class LinqIndexEntry
-		{
-			public string[] Words { get; set; }
-			public Movie Movie { get; set; }
-		}
+class Movie
+{
+	public string Type { get; set; }
+	public string Id { get; set; }
+	public int Version { get; set; }
+	public string Lang { get; set; }
+	public Fields Fields { get; set; }
+}
 
-		class Movie
-		{
-			public string Type { get; set; }
-			public string Id { get; set; }
-			public int Version { get; set; }
-			public string Lang { get; set; }
-			public Field Fields { get; set; }
-		}
-
-		class Field
-		{
-			public string Title { get; set; }
-			public int? Year { get; set; }
-			public string Director { get; set; }
-			public string[] Genere { get; set; }
-			public string[] Actor { get; set; }
-		}
-	}
+class Fields
+{
+	public string Title { get; set; }
+	public int? Year { get; set; }
+	public string Director { get; set; }
+	public string[] Genre { get; set; }
+	public string[] Actor { get; set; }
 }
